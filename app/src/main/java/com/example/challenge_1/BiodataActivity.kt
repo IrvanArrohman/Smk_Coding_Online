51d7fbfcc753357d8bf49d0dee46ab6919bcdac0
IrvanArrohman <irvanngalam32@gmail.com>
2020-05-04 15:21:42 +0700

    selesai

--- app/src/main/java/com/example/challenge_1/BiodataActivity.kt
+++ app/src/main/java/com/example/challenge_1/BiodataActivity.kt
@@ -1,8 +1,10 @@
 package com.example.challenge_1
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
@@ -15,6 +17,8 @@ class BiodataActivity : AppCompatActivity() {
     private lateinit var alamatTxt : TextView
     private lateinit var edit : Button
     private lateinit var about : Button
+    private lateinit var dial : Button
+    internal var call : String?=""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,20 +32,36 @@ class BiodataActivity : AppCompatActivity() {
         alamatTxt = findViewById(R.id.alamatTxt)
         edit = findViewById(R.id.edit)
         about = findViewById(R.id.about)
+        dial = findViewById(R.id.dial)
 
         namaTxt.setText(intent.getStringExtra("data1"))
+
         umurTxt.setText(intent.getStringExtra("data3"))
         emailTxt.setText(intent.getStringExtra("data4"))
         telpTxt.setText(intent.getStringExtra("data5"))
         alamatTxt.setText(intent.getStringExtra("data6"))
+        jkTxt.setText(intent.getStringExtra("data2"))
+
+
 
         edit.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
+            intent.putExtra("data1",namaTxt.text.toString())
+            intent.putExtra("data3",umurTxt.text.toString())
+            intent.putExtra("data4",emailTxt.text.toString())
+            intent.putExtra("data5",telpTxt.text.toString())
+            intent.putExtra("data6",alamatTxt.text.toString())
+            intent.putExtra("data2",jkTxt.text.toString())
             startActivity(intent)
         }
         about.setOnClickListener {
             val intent = Intent(this, AboutActivity::class.java)
             startActivity(intent)
         }
+        dial.setOnClickListener(View.OnClickListener {
+            call = telpTxt.text.toString().trim()
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+Uri.encode(call)))
+            startActivity(intent)
+        })
     }
 }
--- app/src/main/java/com/example/challenge_1/HomeActivity.kt
+++ app/src/main/java/com/example/challenge_1/HomeActivity.kt
@@ -1,11 +1,13 @@
 package com.example.challenge_1
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+
+import android.view.View
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_biodata.*
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -15,7 +17,7 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var telp : EditText
     private lateinit var alamat : EditText
     private lateinit var simpan : Button
-
+    internal lateinit var spinner : Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -26,6 +28,19 @@ class HomeActivity : AppCompatActivity() {
         telp = findViewById(R.id.telp)
         alamat = findViewById(R.id.alamat)
         simpan = findViewById(R.id.simpan)
+        spinner = findViewById(R.id.jk)
+
+        val jk_item = arrayOf("Laki-laki", "Perempuan")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, jk_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
+        spinner.adapter = adapter
+
+
+        nama.setText(intent.getStringExtra("data1"))
+        umur.setText(intent.getStringExtra("data3"))
+        email.setText(intent.getStringExtra("data4"))
+        telp.setText(intent.getStringExtra("data5"))
+        alamat.setText(intent.getStringExtra("data6"))
 
 
 
@@ -36,7 +51,11 @@ class HomeActivity : AppCompatActivity() {
             intent.putExtra("data4",email.text.toString())
             intent.putExtra("data5",telp.text.toString())
             intent.putExtra("data6",alamat.text.toString())
+            intent.putExtra("data2",spinner.selectedItem.toString())
             startActivity(intent)
         }
+
     }
+
+
 }
